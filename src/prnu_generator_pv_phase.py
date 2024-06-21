bc653319af636998af705e78cbeb7cee23dae0f8
janmejoy <janmejoysarker@gmail.com>
2024-06-21 19:26:05 +0530

    Ran once

--- src/prnu_generator_pv_phase.py
+++ src/prnu_generator_pv_phase.py
@@ -67,27 +67,35 @@ def lighten(image_list):
 
 #finds degree of correction implemented by PRNU correction.
 def calib_stats(single_filename, prnu_file, croprow, cropcol, size, name):
-    single= fits.open(single_filename)[0].data
+    single= fits.open(single_filename)[0].data #convert to photoelectrons
     corrected= single/prnu_file
     crop_raw= single[croprow:croprow+size, cropcol:cropcol+size]
     crop_cor= corrected[croprow:croprow+size, cropcol:cropcol+size]
     crop_prnu= prnu_file[croprow:croprow+size, cropcol:cropcol+size]
     
-    sd_raw= np.std(crop_raw)/np.mean(crop_raw)
-    sd_cor= np.std(crop_cor)/np.mean(crop_cor)
-    sd_prnu= np.std(crop_prnu)/np.mean(crop_prnu)
+    sd_raw= np.std(crop_raw)
+    sd_cor= np.std(crop_cor)
+    sd_prnu= np.std(crop_prnu)
     calc_cor= np.sqrt(np.abs(sd_raw**2-sd_prnu**2))
+    ### PERCENT ERRORS ###
+    sd_raw_pc= sd_raw*100/np.mean(crop_raw)
+    sd_cor_pc= sd_cor*100/np.mean(crop_cor)
+    sd_prnu_pc= sd_prnu*100/np.mean(crop_prnu)
+    calc_cor_pc= calc_cor*100/np.mean(crop_cor)
+    
     print(f"\n{name}")
-    print("[1] Sdev Raw:", sd_raw)
-    print("[2] Sdev Corrected:", sd_cor)
-    print("[3] Sdev PRNU: ", sd_prnu)
-    print("[4] sqrt(Raw^2-PRNU^2): ", calc_cor)
+    print("[1] Sdev Raw:", sd_raw_pc)
+    print("[2] Sdev Corrected:", sd_cor_pc)
+    print("[3] Sdev PRNU: ", sd_prnu_pc)
+    print("[4] sqrt(Raw^2-PRNU^2): ", calc_cor_pc)
     print("Avg counts=", np.mean(crop_raw))
     print("Upon good FF correction, [2] and [4] should match well \n")
+    print("Mean", np.mean(crop_cor))
 
 if __name__=='__main__':
     project_path= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/flat_field/LED/onboard_PRNU_project/')
     filelist= glob.glob(project_path+"data/raw/SUT*")
+    save= False
     sav= os.path.join(project_path, 'products/')
     mfg= str(datetime.date.today()) #manufacturing date
     
@@ -114,27 +122,30 @@ if __name__=='__main__':
             aa_355.append(file)
 
     print(f'{date}_prnu_355_ff')
-    prnu_355_ff = flat_generator(ff_355, kernel_355, f'{date}_prnu_355_ff', save=True)
-    calib_stats(ff_355[1], prnu_355_ff, 2000, 1500, 25, f'{date}_prnu_355_ff')
+    prnu_355_ff = flat_generator(ff_355, kernel_355, f'{date}_prnu_355_ff', save)
     
     print(f'{date}_prnu_355_aa')
-    prnu_355_aa = flat_generator(aa_355, kernel_355, f'{date}_prnu_355_aa', save=True)
-    calib_stats(aa_355[1], prnu_355_aa, 2000, 1500, 25, f'{date}_prnu_355_aa')
+    prnu_355_aa = flat_generator(aa_355, kernel_355, f'{date}_prnu_355_aa', save)
     
     prnu_355_common= lighten([prnu_355_ff, prnu_355_aa])
     sav_hdu= fits.PrimaryHDU(prnu_355_common, header= prep_header('prnu_355_common.fits', mfg, date))
-    sav_hdu.writeto(f'{sav}prnu_355_common.fits')
+    if save: sav_hdu.writeto(f'{sav}{date}_prnu_355_common.fits')
+    calib_stats(aa_355[0], prnu_355_common, 2200, 1500, 25, f'{date}_prnu_355_aa') #2200, 1500, 25,
+    calib_stats(ff_355[0], prnu_355_common, 2200, 1500, 25, f'{date}_prnu_355_ff')
+
     
     print(f'{date}_prnu_255_ff')
-    prnu_255_ff = flat_generator(ff_255, kernel_255, f'{date}_prnu_255_ff', save=True)
-    calib_stats(ff_255[1], prnu_255_ff, 3700, 2000, 25, f'{date}_prnu_255_ff')
+    prnu_255_ff = flat_generator(ff_255, kernel_255, f'{date}_prnu_255_ff', save)
     
     print(f'{date}_prnu_255_aa')
-    prnu_255_aa = flat_generator(aa_255, kernel_255, f'{date}_prnu_255_aa', save=True)
-    calib_stats(aa_255[1], prnu_255_aa, 2000, 1500, 25, f'{date}_prnu_255_aa')
+    prnu_255_aa = flat_generator(aa_255, kernel_255, f'{date}_prnu_255_aa', save)
     
     prnu_255_common= lighten([prnu_255_ff, prnu_255_aa])
     sav_hdu= fits.PrimaryHDU(prnu_255_common, header= prep_header('prnu_255_common.fits', mfg, date))
-    sav_hdu.writeto(f'{sav}prnu_255_common.fits')
+    if save: sav_hdu.writeto(f'{sav}{date}_prnu_255_common.fits')
+    calib_stats(ff_255[0], prnu_255_common, 2000, 3800, 25, f'{date}_prnu_255_ff')
+    calib_stats(aa_255[0], prnu_255_common, 2000, 3800, 25, f'{date}_prnu_255_aa')
+
+
     
     
